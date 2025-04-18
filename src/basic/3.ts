12ed7275973353a19b2ce3924323bb9e9f9685e9
Plane-Jumper5 <planejumper2025@gmail.com>
2025-04-18 23:51:43 +0300

    last push

---
+++ src/basic/3.ts
@@ -0,0 +1,8 @@
+let value: string | number;
+value = 'Hello'; 
+value = 123;     
+
+
+let stat: 'enable' | 'disable';
+stat = 'enable';  
+stat = 'disable'; 
---
+++ src/basic/4.ts
@@ -0,0 +1,8 @@
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
