6615439cb93bf7a811ac81fec613da889ba49e5a
Plane-Jumper5 <planejumper2025@gmail.com>
2025-04-19 23:25:29 +0300

    add customError

--- src/basic/4.ts
+++ src/basic/4.ts
@@ -6,3 +6,8 @@ function showMessage(message: string): void {
 function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
+
+
+function customError(): never {
+  throw new Error('Error');
+}
